bbd5b82accf4f61017acf8fea08c864714c6b9b0
Matheus Pires <matheus.h98@hotmail.com.br>
2021-11-05 18:06:45 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div> 
+    <>
+    <NavBar />
+    <div className="container"> 
       <h1 className="text-primary"> Olá mundo!</h1>
-
     </div>
+    <Footer />
+    </>
   );
 }
 
